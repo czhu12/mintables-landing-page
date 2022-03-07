3af25a5b0d7f1b3cd89d12c055f34418b46a1b68
Chris Zhu <chriszhu@Chriss-MacBook-Pro.local>
2022-03-07 01:02:07 -0800

    initial commit

---
+++ pages/_app.js
@@ -0,0 +1,10 @@
+import '../styles/overrides.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '../styles/globals.css'
+import '../styles/app.scss'
+
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />
+}
+
+export default MyApp

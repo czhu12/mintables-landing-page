8e009fd02cfb3a263b015eab17046311cbfcfa8e
Chris Zhu <chriszhu@Chriss-MacBook-Pro.local>
2022-03-07 21:59:01 -0800

    added 404 redirect

---
+++ pages/404.js
@@ -0,0 +1,14 @@
+const APP_ROUTE = "https://app.mintables.club"
+
+const Redirect = () => {
+  if (typeof window !== "undefined") {
+    // Client-side-only code
+    if (window.location.pathname !== "/") {
+      window.location.href = APP_ROUTE + window.location.pathname
+    }
+  }
+
+  return <p></p>
+}
+
+export default Redirect
--- pages/index.js
+++ pages/index.js
@@ -3,15 +3,7 @@ import { RoughNotation, RoughNotationGroup } from "react-rough-notation";
 import Head from "next/head";
 //import Image from "next/image";
 
-const APP_ROUTE = "https://app.mintables.club"
-
 export default function Home() {
-  if (typeof window !== "undefined") {
-    // Client-side-only code
-    if (window.location.pathname !== "/") {
-      window.location.href = APP_ROUTE + window.location.pathname
-    }
-  }
   return (
     <div>
       <Head>
